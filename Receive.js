d4f2ceb4c5ffc776082f51c604ed6c8c3ac09ef8
Pradyumna Gayake <55043418+prgayake@users.noreply.github.com>
2022-03-18 12:51:58 +0530

    Add files via upload

---
+++ Receive.js
@@ -0,0 +1,31 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var queueURL = "https://sqs.ap-south-1.amazonaws.com/000263543451/TestQueue";
+
+var params = {
+ AttributeNames: [
+    "SentTimestamp"
+ ],
+ MaxNumberOfMessages: 10,
+ MessageAttributeNames: [
+    "All"
+ ],
+ QueueUrl: queueURL,
+ VisibilityTimeout: 20,
+ WaitTimeSeconds: 0
+};
+
+sqs.receiveMessage(params, function(err, data) {
+  if (err) {
+    console.log("Receive Error", err);
+  } else if (data.Messages) {
+    console.log("Success :", data.Messages[0].MessageAttributes.Title.StringValue);
+   
+  }
+});
